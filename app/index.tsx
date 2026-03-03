9b965cdfc5b8db0a3ad4a38b25c178b9caa202ed
Axel0rtiz <113001587+Axel0rtiz@users.noreply.github.com>
2026-03-02 20:11:39 -0600

    Add fetc get pokemon

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,41 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Image, Text, View } from "react-native";
 
 export default function Index() {
+  useEffect(() => {
+    console.log("En pantalla");
+    getPokemons();
+  }, []);
+
+  const [pokemon, setPokemon] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  const getPokemons = async () => {
+    const url: string =
+      "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    const response = await fetch(url, {
+      method: "GET",
+    });
+    const data = await response.json();
+    const firstPokemonUrl = data.results[0].url;
+    const pokemonResponse = await fetch(firstPokemonUrl);
+    const pokemonData = await pokemonResponse.json();
+    setPokemon(pokemonData);
+    setLoading(false);
+    console.log(pokemonData);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      {pokemon && (
+        <>
+          <Image
+            source={{ uri: pokemon.sprites.front_default }}
+            style={{ width: 150, height: 150 }}
+          />
+          <Text>{pokemon?.name?.toUpperCase()}</Text>
+        </>
+      )}
     </View>
   );
 }
